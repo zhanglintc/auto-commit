867a2b94822b70cab4b1f67a813f09e7b6811887
zhanglintc <zhanglintc623@gmail.com>
2018-08-04 17:16:12 +0800

    MmrzLog

--- caller.py
+++ caller.py
@@ -3,17 +3,26 @@
 
 import os, sys
 import GitHubReminder
+import traceback
+
+from MmrzLog import log
 
 #command = r"python {}\GitHubReminder.py".format(sys.path[0])
 
 # if GitHubReminder.github_reminder() returned with not code 0, recall it
 # Zhanglin_Github_URL = "https://github.com/zhanglintc?tab=overview&from={0}".format(GitHubReminder.TODAY)
-Zhanglin_Github_URL = "https://github.com/zhanglintc"
-for i in range(3):
-    if GitHubReminder.github_reminder(GITHUB_URL = Zhanglin_Github_URL, Auto_Commit_Flag = True):
-        print('zhanglin failed, try again\n')
-    else:
-        break
+try:
+    raise RuntimeError('testError')
+    Zhanglin_Github_URL = "https://github.com/zhanglintc"
+    for i in range(3):
+        if GitHubReminder.github_reminder(GITHUB_URL = Zhanglin_Github_URL, Auto_Commit_Flag = True):
+            print('zhanglin failed, try again\n')
+        else:
+            break
+except Exception as e:
+    log.e(e)
+    exstr = traceback.format_exc()
+    log.e(exstr)
 
 # exit after zhanglin, not execute any more
 sys.exit()
